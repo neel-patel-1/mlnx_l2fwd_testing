788ba3fad2955d2cbbb141443185b052dca70e80
Derrick Quinn <d552q900@pollux.ittc.ku.edu>
2023-05-18 10:51:01 -0500

    added Pktgen install script

---
+++ Pktgen-DPDK.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#need this for $ROOT
+source ./shared.sh
+
+# remove old submodules
+rm -rf Pktgen-DPDK 
+
+
+# Initialize pktgen module
+git submodule init
+git submodule update --recursive
+
+cd Pktgen-DPDK
+git checkout pktgen-21.11.0
+
+export RTE_SDK=../dpdk
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+export PKTGEN_DIR=$(pwd)
+export PKG_CONFIG_PATH=/scratch/pding/opt/usr/local/lib64/pkgconfig:$PKG_CONFIG_PATH
+
+make -j
