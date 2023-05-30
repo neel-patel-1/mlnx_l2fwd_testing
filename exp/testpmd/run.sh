fd5742932799437dfb86dae9110e923eddf37f02
Derrick Quinn <d552q900@altra.ittc.ku.edu>
2023-05-30 11:02:58 -0500

    l2fwd script

--- exp/testpmd/run.sh
+++ exp/testpmd/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ../../shared.sh
 
-PMD=$DPDK_BUILD/app/dpdk-testpmd
-mkdir logs
+PMD=$DPDK_BUILD/examples/dpdk-l2fwd
+mkdir cfgs
 
 FWD_CORES=1
 LOGICAL=80-82
@@ -12,9 +12,10 @@ PORTS=0x0001
 RXQ_PER_LCORE=8
 
 i=0
-#RECIEVED=$( $PMD -l $LOGICAL -n $CHANS -- --forward-mode=macswap --portmask=$PORTS --num-procs=$FWD_CORES --nb-cores=$FWD_CORES | grep -o 'TX-packets: [0-9]*' | head -1 | grep -o '[0-9]*')
+dur=$( bc -l <<< "2 + 1.5 * $1")
+timeout --foreground $dur $PMD -l $LOGICAL -n $CHANS -- -p $PORTS -T 1 | grep  'Packets sent: [0-9]*' 2> /dev/null
 
-#echo $1,$2,$3,$RECIEVED >> results.csv
 
-perf stat -B -e L1-icache-load,L1-icache-load-misses,L1-dcache-load,L1-dcache-load-misses,cycles,instructions,stall_backend,stall_frontend,mem_access $PMD -l $LOGICAL -n $CHANS -- --forward-mode=macswap --portmask=$PORTS --num-procs=$FWD_CORES --nb-cores=$FWD_CORES 
+
+
 
